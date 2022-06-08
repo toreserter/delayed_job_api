d8e7ae4e70f14a2b0542502c218787113bf40883
Töre Serter <toreserter@gmail.com>
2022-06-08 21:47:23 +0300

    fix

--- lib/generators/delayed_job_api/install_generator.rb
+++ lib/generators/delayed_job_api/install_generator.rb
@@ -1,7 +1,7 @@
 module DelayedJobApi
   class InstallGenerator < Rails::Generators::Base
     desc 'Installs DelayedJobApi'
-    source_root File.expand_path("../../templates", __FILE__)
+    source_root File.expand_path("../templates", __FILE__)
 
     def install
       template "delayed_job_api.rb", "config/initializers/delayed_job_api.rb"
